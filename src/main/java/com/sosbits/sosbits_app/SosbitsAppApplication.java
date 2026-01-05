dd96b0927d709cd2b373c3dc0078ff88b78d67d5
Arthur Folly <arthurfollynf@gmail.com>
2026-01-05 16:15:31 -0300

    Primeiro commit, organizando as pastas e arquitetura do projeto

---
+++ src/main/java/com/sosbits/sosbits_app/SosbitsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sosbits.sosbits_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosbitsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosbitsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sosbits/sosbits_app/SosbitsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosbits.sosbits_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosbitsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
